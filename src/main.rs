511def966b983775b38a8bfae6d0d14b40e957c9
Edwin <Edwin.v.velthoven@gmail.com>
2024-10-22 21:57:30 +0200

    gave myself brain damage or expansion?

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
  
 fn main() {
     // default: 10.001
-    let limit = 20000;
+    let limit = 1000000000;
     let result = sieve_of_atkin(limit);
     let mut prime_list: Vec<usize> = Vec::new();
 
@@ -14,33 +14,27 @@ fn main() {
 
 }
 
-// !! This is mostly a interpretation of a explanation (I din't really understand) of sieve of atkin.
+// !! This is mostly a interpretation of a explanation (I didn't really understand) of sieve of atkin.
 // !! I don't understand what the hell is happening...
 fn sieve_of_atkin(limit: usize) -> Vec<usize> {
     let mut is_prime = vec![false; limit + 1];
     let sqrt_limit = (limit as f64).sqrt() as usize;
 
-    // Alg step 1
-    for x in 1..=sqrt_limit {
-        for y in (1..=sqrt_limit).step_by(2) {
+    // ! The algorithm completely ignores any numbers with remainder modulo 60 that is divisible by two, three, or five,
+    // ! since numbers with a modulo 60 remainder divisible by one of these three primes are themselves divisible by that prime.
+
+    // Alg step 1 | Solution to: 4*x.pow2 + y.pow2 = n is odd and the number is squarefree 
+    // ? Testing with iterators instead of explicit for loops | No performance difference
+    (1..=sqrt_limit).for_each(|x| {
+        (1..=sqrt_limit).step_by(2).for_each(|y| {
             let n = 4 * x.pow(2) + y.pow(2);
             if n <= limit && matches!(n % 60, 1 | 13 | 17 | 29 | 37 | 41 | 49 | 53) {
                 is_prime[n] = !is_prime[n];
             }
-        }
-    }
-    // Alg step 2
-    for x in (2..=sqrt_limit).step_by(2) {
-        for y in (2..=sqrt_limit).step_by(2) {
-            let n = 3 * x.pow(2) + y.pow(2);
-            if n <= limit && matches!(n % 60, 7 | 19 | 31 | 43) {
-                // sets n in prime too false, if it was true
-                is_prime[n] = !is_prime[n];
-            }
-        }
-    }
+        });
+    });
 
-    // Alg step 3
+    // Alg step 3 | Solution to: 3x.pow2 + y.pow(2) = n is odd and n is squarefree
     for x in 2..=sqrt_limit {
         for y in (1..x).step_by(2) {
             let n = 3 * x.pow(2) * y.pow(2);
@@ -53,9 +47,7 @@ fn sieve_of_atkin(limit: usize) -> Vec<usize> {
     // Eliminate composites by sieving
     for n in 7..sqrt_limit {
         if is_prime[n] {
-            let n2 = n.pow(2);
-            // TODO: Check if can be shorter
-            for k in (n2..=limit).step_by(n2) {
+            for k in (n.pow(2)..=limit).step_by(n.pow(2)) {
                 is_prime[k] = false;
             }
         }
